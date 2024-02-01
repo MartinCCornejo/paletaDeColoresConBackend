f5fa3e509cf900f547c95bc125310d678f6d881d
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-01 01:44:45 -0300

    Bajamos el footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-black text-center py-3 px-1 text-light'>
+            <p>&copy; Martín Cornejo - Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/FormularioColores.jsx
@@ -0,0 +1,9 @@
+import { Form,Button } from "react-bootstrap";
+
+const FormularioColores = () => {
+    return (
+        <Form></Form>
+    );
+};
+
+export default FormularioColores;
