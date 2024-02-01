cd0a562e3638b4af3fe0ed2601687838b8e439ee
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-01 01:36:44 -0300

    Agregamos el titulo del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
 
   return (
-   <>
-   </>
+    <>
+    <header className='bg-danger text-center text-light  py-3'>
+      <h1 className='display-5'>TP React <br></br> Ejercicio 6 - Paleta de colores</h1>
+    </header>
+    </>
   )
 }
 
