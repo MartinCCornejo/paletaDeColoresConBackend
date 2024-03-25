e3e6dc2194d358fd94acb1fa490afbb52d8608d9
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-03-25 12:24:46 -0300

    Rellenamos los datos con los colores agregados en la api

--- src/components/ItemColor.jsx
+++ src/components/ItemColor.jsx
@@ -1,13 +1,13 @@
 import { Button, Col } from "react-bootstrap";
 
-const ItemColor = () => {
+const Color = ({color}) => {
 
   return (
     <Col md="3" lg="2" className="box-shadow py-3">
       <div className="text-center">
-        <h3 className="fs-4 text-capitalize"></h3>
+        <h3 className="fs-4 text-capitalize">{color.nombreColor}</h3>
         <div className="bg-gray py-3 my-2">
-            <div className="color-item"></div>
+            <div className="color-item" style={{backgroundColor: `#${color.codigoColor}`}} ></div>
         </div>
         <Button variant="danger">
           <i className="bi bi-trash fs-5"></i>
@@ -17,4 +17,4 @@ const ItemColor = () => {
   );
 };
 
-export default ItemColor;
+export default Color;
