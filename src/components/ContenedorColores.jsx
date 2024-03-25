aee7e8fe6c8d6a7bb149c23c51dc65c45b4081c8
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-03-25 10:31:58 -0300

    Modifique el formulario para agregar un input que pida el codigo del color que vamos a agregar

--- src/components/ContenedorColores.jsx
+++ src/components/ContenedorColores.jsx
@@ -1,17 +1,17 @@
 import { Row } from "react-bootstrap";
 import Color from "./Color";
 
-const ContenedorColores = ({arrayColores,borrarColor}) => {
+const ContenedorColores = () => {
 
-    if (arrayColores.length === 0) {
-        return <p className="text-center lead text-secondary fs-2">No hay colores almacenados</p>
-    }
+    // if (arrayColores.length === 0) {
+    //     return <p className="text-center lead text-secondary fs-2">No hay colores almacenados</p>
+    // }
 
   return (
     <section>
     <Row className="gap-5 justify-content-center align-item-center">
         {
-            arrayColores.map((elemento,posicion) => <Color borrarColor={borrarColor} key={posicion} color={elemento}></Color>)
+            // arrayColores.map((elemento,posicion) => <Color></Color>)
         }
     </Row>
     </section>
--- src/components/FormularioColores.jsx
+++ src/components/FormularioColores.jsx
@@ -4,137 +4,138 @@ import Swal from "sweetalert2";
 import ContenedorColores from "./ContenedorColores";
 
 const FormularioColores = () => {
-  const [color, setColor] = useState("");
-  const coloresLocalStorage = JSON.parse(localStorage.getItem('coloresLocal')) || [];
-  const [arrayColores, setArrayColores] = useState(coloresLocalStorage);
+  // const [color, setColor] = useState("");
+  // const coloresLocalStorage = JSON.parse(localStorage.getItem('coloresLocal')) || [];
+  // const [arrayColores, setArrayColores] = useState(coloresLocalStorage);
 
-  useEffect(()=>{
-    localStorage.setItem('coloresLocal',JSON.stringify(arrayColores));
-  },[arrayColores,color])
+  // useEffect(()=>{
+  //   localStorage.setItem('coloresLocal',JSON.stringify(arrayColores));
+  // },[arrayColores,color])
 
 
 // Funcion para verificar que el color sea valido 
-  const esColorValido = (colorInput) => {
-    // Lista de nombres de colores en inglés
-    const coloresValidos = [
-      "black", "silver", "gray", "white", "maroon", "red", "purple", "fuchsia",
-      "green", "lime", "olive", "yellow", "navy", "blue", "teal", "aqua",
-      "orange", "aliceblue", "antiquewhite", "aquamarine", "azure", "beige",
-      "bisque", "blanchedalmond", "blueviolet", "brown", "burlywood", "cadetblue", "cyan", "lightblue"
-    ];
+  // const esColorValido = (colorInput) => {
+  //   // Lista de nombres de colores en inglés
+  //   const coloresValidos = [
+  //     "black", "silver", "gray", "white", "maroon", "red", "purple", "fuchsia",
+  //     "green", "lime", "olive", "yellow", "navy", "blue", "teal", "aqua",
+  //     "orange", "aliceblue", "antiquewhite", "aquamarine", "azure", "beige",
+  //     "bisque", "blanchedalmond", "blueviolet", "brown", "burlywood", "cadetblue", "cyan", "lightblue"
+  //   ];
   
-    // Verifica si el color ingresado está en la lista de colores válidos
-    const esValido = coloresValidos.includes(colorInput.toLowerCase());
+  //   // Verifica si el color ingresado está en la lista de colores válidos
+  //   const esValido = coloresValidos.includes(colorInput.toLowerCase());
   
-    if (!esValido) {
-      Swal.fire({
-        title: "Error",
-        text: "Por favor, ingrese un nombre de color válido en español.",
-        icon: "error",
-      });
-    }
+  //   if (!esValido) {
+  //     Swal.fire({
+  //       title: "Error",
+  //       text: "Por favor, ingrese un nombre de color válido en español.",
+  //       icon: "error",
+  //     });
+  //   }
   
-    return esValido;
-  };
+  //   return esValido;
+  // };
   
-// Funcion que verifica si un color es repetido 
-  const esColorRepetido = (inputColor) => {
-    for (let i = 0; i < arrayColores.length; i++) {
-       if (arrayColores.includes(color)) {
-        return true
-       }
+// // Funcion que verifica si un color es repetido 
+//   const esColorRepetido = (inputColor) => {
+//     for (let i = 0; i < arrayColores.length; i++) {
+//        if (arrayColores.includes(color)) {
+//         return true
+//        }
 
-       return false
-    }
-  }
+//        return false
+//     }
+//   }
   
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-    if (!esColorValido(color)) {
-        Swal.fire({
-          title: "Error",
-          text: "Por favor, ingrese un color válido.",
-          icon: "error",
-        });
-        return;
-    } if (esColorRepetido(color)) {
-        Swal.fire({
-            title: "Error",
-            text: "Ya ingresaste este color!",
-            icon: "error",
-          });
-          return;
-    }
+  //   if (!esColorValido(color)) {
+  //       Swal.fire({
+  //         title: "Error",
+  //         text: "Por favor, ingrese un color válido.",
+  //         icon: "error",
+  //       });
+  //       return;
+  //   } if (esColorRepetido(color)) {
+  //       Swal.fire({
+  //           title: "Error",
+  //           text: "Ya ingresaste este color!",
+  //           icon: "error",
+  //         });
+  //         return;
+  //   }
 
-    setArrayColores([...arrayColores, color]);
 
-    Swal.fire({
-      title: "Bien hecho!",
-      text: "El color se agrego correctamente",
-      icon: "success",
-    });
+  //   Swal.fire({
+  //     title: "Bien hecho!",
+  //     text: "El color se agrego correctamente",
+  //     icon: "success",
+  //   });
 
-    setColor("");
-  };
+  // };
 
-  const borrarColor = (nombreDelColor) => {
-    // Borramos la tarea con el metodo filter
-    const nuevoArrayColores = arrayColores.filter(
-      (elemento) => elemento !== nombreDelColor
-    );
+  const borrarColor = () => {
 
-    Swal.fire({
-      title: "Estas seguro de borrar este color?",
-      text: "Luego no podras revertir esta acción!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Si, borrar",
-      cancelButtonText: "Cancelar",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Color eliminado!",
-          text: "El color se elimino correctamente.",
-          icon: "success",
-        });
-        // Actualizamos el state arrayTareas
-        setArrayColores(nuevoArrayColores);
-      }
-    });
+    // Swal.fire({
+    //   title: "Estas seguro de borrar este color?",
+    //   text: "Luego no podras revertir esta acción!",
+    //   icon: "warning",
+    //   showCancelButton: true,
+    //   confirmButtonColor: "#3085d6",
+    //   cancelButtonColor: "#d33",
+    //   confirmButtonText: "Si, borrar",
+    //   cancelButtonText: "Cancelar",
+    // }).then((result) => {
+    //   if (result.isConfirmed) {
+    //     Swal.fire({
+    //       title: "Color eliminado!",
+    //       text: "El color se elimino correctamente.",
+    //       icon: "success",
+    //     });
+    //     // Actualizamos el state arrayTareas
+    //   }
+    // });
   };
 
 
 
   return (
     <section>
-      <Form className="my-5 border p-3 box-shadow" onSubmit={handleSubmit}>
+      <Form className="my-5 border p-3 box-shadow" >
         <Form.Group controlId="formularioColor">
           <Form.Label className="fs-4">Administrar colores</Form.Label>
           <Row className="justify-content-center align-items-center bg-gray py-4 gap-3">
             <Col md="3">
-              <div className="box border border-5" style={{backgroundColor: color}}></div>
+              <div className="box border border-5"></div>
             </Col>
             <Col md="7">
+              <div className="mb-3">
+              <Form.Label>Ingrese el nombre del color</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Ingrese un color ej Blue"
-                onChange={(e) => setColor(e.target.value)}
-                value={color}
+                placeholder="Ej: Rojo"
               />
+              </div>
+              <div>
+              <Form.Label>Ingrese el código del color (en hexadecimal)</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ej: FF0000"
+              />
+              </div>
             </Col>
           </Row>
           <div className="text-end">
-            <Button variant="primary" type="submit" className="mt-3" disabled={color.length < 3}>
+            <Button variant="primary" type="submit" className="mt-3">
               Agregar
             </Button>{" "}
           </div>
         </Form.Group>
       </Form>
-      <ContenedorColores arrayColores={arrayColores} borrarColor={borrarColor}></ContenedorColores>
+      <ContenedorColores></ContenedorColores>
     </section>
   );
 };
